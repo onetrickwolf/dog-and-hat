5392babfee43032a097b60f2eb531f10d7b03fed
Brent C <brent.conn@gmail.com>
2022-11-24 14:51:19 -0500

    Initial commit

---
+++ src/liveblocks.config.js
@@ -0,0 +1,42 @@
+import { createClient } from "@liveblocks/client";
+import { createRoomContext } from "@liveblocks/react";
+
+let PUBLIC_KEY =
+  "pk_dev_ir9pOTydCsbaMps7TNqUvkYF0uL9Q4WuPxAOpisULeLammKO54gOXnfKv1wvr0MO";
+
+if (!/^pk_(live|test)/.test(PUBLIC_KEY)) {
+  console.warn(
+    `Replace "${PUBLIC_KEY}" by your public key from https://liveblocks.io/dashboard/apikeys.\n` +
+      `Learn more: https://github.com/liveblocks/liveblocks/tree/main/examples/react-dashboard#getting-started.`
+  );
+}
+
+overrideApiKey();
+
+const client = createClient({
+  publicApiKey: PUBLIC_KEY
+});
+
+export const {
+  suspense: {
+    useHistory,
+    useOthers,
+    useStorage,
+    useMutation,
+    RoomProvider,
+    useSelf
+  }
+} = createRoomContext(client);
+
+/**
+ * This function is used when deploying an example on liveblocks.io.
+ * You can ignore it completely if you run the example locally.
+ */
+function overrideApiKey() {
+  const query = new URLSearchParams(window?.location?.search);
+  const apiKey = query.get("apiKey");
+
+  if (apiKey) {
+    PUBLIC_KEY = apiKey;
+  }
+}
